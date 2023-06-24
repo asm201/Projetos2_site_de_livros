039a63bfacf7efe94ffc4db5813658658a1a0a89
asm201 <asirikaku2000@gmail.com>
2023-06-24 14:15:14 -0300

    deletar Livro completo

--- src/app/usuario/perfil/perfil.component.ts
+++ src/app/usuario/perfil/perfil.component.ts
@@ -17,4 +17,14 @@ export class PerfilComponent implements OnInit {
     const teste = this.livroRepo.find({where:{BD_EMAIL_USUARIO: 'emailteste@teste.com'}}).then((livros)=> (this.livros = livros))
   }
 
+  async deletar(livro: Livros){
+    try {
+      if(confirm("Certeza que deseja deletar este Livro ?")) {
+        await this.livroRepo.delete(livro)
+        this.livros = this.livros.filter((l) => l !== livro)
+      }
+    } catch (error: any) {
+      alert(error.message)
+    }
+  }
 }
