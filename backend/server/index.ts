0230d6b68c1ef9af28afc44edcf3cab77428b59f
DanielG-s <53453411+DanielG-s@users.noreply.github.com>
2023-06-27 23:21:49 -0300

    ajuste api

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,7 +1,17 @@
-import express from 'express'
+import express from 'express';
 import { api } from './api';
 
+const bodyParser = require('body-parser')
 const app = express();
+
+const PORT = process.env['PORT'] || 3002
+
 app.use(api);
 app.get('/api/hi',(req,res) => res.send('Hello'));
-app.listen(3002, () => console.log('Started :)'));
+
+app.use(express.static(process.cwd()+"/dist/forms-reativos"))
+app.listen(PORT, async() => {
+    console.log(`API Gateway running in port ${PORT}!`)
+});
+
+
