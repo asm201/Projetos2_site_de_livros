39fb56df970e5c1d2083871e6c86f4fe83f261d4
DanielG-s <53453411+DanielG-s@users.noreply.github.com>
2023-06-28 21:32:19 -0300

    ajuste api

--- backend/server/api.ts
+++ backend/server/api.ts
@@ -1,8 +1,13 @@
 import { remultExpress } from "remult/remult-express";
 import { Usuario } from "../../src/Shared/Usuario";
 import { Livros } from "src/Shared/Livros";
+import { createPostgresConnection } from 'remult/postgres'
 
 export const api = remultExpress({
     entities:[Usuario,Livros],
+    dataProvider: createPostgresConnection({
+        connectionString: 
+            process.env['DATABASE_URL']
+    }),
 })
 
