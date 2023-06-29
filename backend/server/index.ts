8a5fb142cc66742843c9bef6741498836ce3f755
DanielG-s <53453411+DanielG-s@users.noreply.github.com>
2023-06-28 21:24:18 -0300

    Link com o postgres

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { api } from './api';
+import { createPostgresConnection } from 'remult/postgres'
 
 const bodyParser = require('body-parser')
 const app = express();
@@ -8,6 +9,10 @@ const PORT = process.env['PORT'] || 3002
 
 app.use(api);
 app.get('/api/hi',(req,res) => res.send('Hello'));
+dataProvider: createPostgresConnection({
+    connectionString: 
+        process.env['DATABASE_URL']
+});
 
 app.use(express.static(process.cwd()+"/dist/forms-reativos"))
 app.listen(PORT, async() => {
